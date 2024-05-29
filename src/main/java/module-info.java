5a33550de9af077517c5a6720e5348f988a79980
TortueSandwich <artsaillant@gmail.com>
2024-05-29 18:47:51 +0200

    ajout config vscode

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module local.furthestpointoptimization {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
 
     opens local.furthestpointoptimization to javafx.fxml;
